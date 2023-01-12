bfbdf5e2ec1ad2bbdb64ebed06d5320ab06160f0
Adisdas <adis.buljugbasi@student.uninp.edu.rs>
2023-01-12 22:21:37 +0100

    modified:   client/src/pages/Single.jsx

--- api/index.js
+++ api/index.js
@@ -1,16 +1,15 @@
 import  express from "express"
-import authRoutes from "./routes/posts.js";
-import userRoutes from "./routes/posts.js";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
-
 const app = express();
 
 app.use(express.json())
-app.use("/api/auth", postRoutes)
-app.use("/api/users", postRoutes)
-app.use("/api/posts", postRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes); 
 
 app.listen(8800, () => {
-    console.log("Listening to port 8800!")
+    console.log("Listening to port 8800!,ZNACI RADI")
 })
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import axios from "axios";
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
 
-  const posts = [
+  const cat = useLocation().search
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/posts${cat}`);
+        setPosts(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [cat]);
+/*   const posts = [
     {
       id: 1,
       title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
@@ -28,27 +45,26 @@ const Home = () => {
       desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
       img: "https://images.pexels.com/photos/6157049/pexels-photo-6157049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     },
-  ];
+  ]; */
   return (
-    <div className='home'>
-<div className="posts">
-  {posts.map((post) => (
-    <div className="post" key={post.id}>
-      <div className="img">
-        <img src={post.img} alt="" />
-      </div>
-      <div className="content">
-      <Link className="link" to={`/post/${post.id}`}>
-        <h1>{post.title}</h1>
-        <p>{post.desc}</p>
-        <button>Read More</button>
-
-        </Link>
+    <div className="home">
+      <div className="posts">
+        {posts.map((post) => (
+          <div className="post" key={post.id}>
+            <div className="img">
+              <img src={`../upload/${post.img}`} alt="" />
+            </div>
+            <div className="content">
+              <Link className="link" to={`/post/${post.id}`}>
+                <h1>{post.title}</h1>
+              </Link>
+              <p>{getText(post.desc)}</p>
+              <button>Read More</button>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
-  ))}
-  </div>      
-</div>
   );
 };
 
